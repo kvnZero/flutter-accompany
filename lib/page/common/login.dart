f040cf87d3e334954c6e517e86b1907eb91a396f
abigeater <abigeater@163.com>
2020-02-07 16:03:24 +0800

    login页面样式

--- lib/page/common/login.dart
+++ lib/page/common/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:accompany/data/models/auth.dart';
 import 'package:toast/toast.dart';
+import 'package:accompany/page/common/reg.dart';
 
 class LoginScreen extends StatefulWidget{
   @override
@@ -26,57 +27,67 @@ class LoginScreenState extends State<LoginScreen>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(appBar: AppBar(title: Text('Second page'),),
+    return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width/1.5,
-            child:Image.asset('images/logo.png',fit: BoxFit.fill,),
-          ),
-          Center(
-            child: Form(
-              key: _formKey,
-                autovalidate: true,
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                        autofocus: true,
-                        controller: _unameController,
-                        decoration: InputDecoration(
-                            hintText: "手机号",
-                            icon: Icon(Icons.person)
-                        ),
-                        validator: (v) {
-                          return v.trim().length > 0 ? null : "手机号不能为空";
-                        }
-                    ),
-                    TextFormField(
-                        controller: _pwordController,
-                        decoration: InputDecoration(
-                            hintText: "登录密码",
-                            icon: Icon(Icons.lock)
-                        ),
-                        obscureText: true,
-                        validator: (v) {
-                          return v.trim().length > 6 ? null : "密码不能少于6位";
-                        }
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 28.0,left: 10,right: 10),
-                      child: Row(
+          Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width/1.5,
+                child:Image.asset('images/logo.png',fit: BoxFit.fill,),
+              ),
+              Container(
+                  child: Form(
+                      key: _formKey,
+                      autovalidate: false,
+                      child: Column(
                         children: <Widget>[
-                          Consumer<AuthModel>(
-                            builder: (context, user, child) {
-                              return Expanded(
-                                child: RaisedButton(
-                                  padding: EdgeInsets.all(15.0),
-                                  child: Text("登录账户"),
-                                  color: Theme.of(context).primaryColor,
+                          Container(
+                            width: MediaQuery.of(context).size.width/1.3,
+                            child: TextFormField(
+                                autofocus: true,
+                                controller: _unameController,
+                                decoration: InputDecoration(
+                                  hintText: "手机号",
+                                  icon: Icon(Icons.person,),
+                                  contentPadding: EdgeInsets.only(left: 15,right: 15),
+                                  border:OutlineInputBorder(
+                                    borderRadius: BorderRadius.vertical(top: Radius.circular(8)),
+                                  ),
+                                ),
+                                style: TextStyle(fontSize: 14),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top:0),
+                            width: MediaQuery.of(context).size.width/1.3,
+                            child: TextFormField(
+                                controller: _pwordController,
+                                decoration: InputDecoration(
+                                  hintText: "登录密码",
+                                  icon: Icon(Icons.lock),
+                                  contentPadding: EdgeInsets.only(left: 15,right: 15),
+                                  border:OutlineInputBorder(
+                                      borderRadius: BorderRadius.vertical(bottom: Radius.circular(8))
+                                  ),
+                                ),
+                                style: TextStyle(fontSize: 14),
+                                obscureText: true,
+                            ),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width/1.3,
+                            height: 40,
+                            margin: const EdgeInsets.only(top: 10.0),
+                            child: Consumer<AuthModel>(
+                              builder: (context, user, child) {
+                                return RaisedButton(
+                                  color: Colors.blue[400],
+                                  child: Text("登  录",style: TextStyle(fontSize: 16),),
                                   textColor: Colors.white,
                                   onPressed: () {
-                                    if((_formKey.currentState as FormState).validate()){
-                                      //验证通过提交数据
+                                    if(_unameController.text.trim().length > 0 && _pwordController.text.trim().length >= 3){
                                       Future<String> msg = user.login(username: _unameController.text, password: _pwordController.text);
                                       msg.then((v){
                                         if (v.isNotEmpty) {
@@ -84,18 +95,67 @@ class LoginScreenState extends State<LoginScreen>{
                                           Toast.show(v, context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
                                         }
                                       });
+                                    }else{
+                                      //如果输入有误 toast错误
+                                      Toast.show("请检查输入", context, duration: Toast.LENGTH_SHORT, gravity:  Toast.BOTTOM);
                                     }
                                   },
-                                ),
-                              );
-                            },
+                                );
+                              }
+                            ),
                           ),
                         ],
-                      ),
-                    )
+                      ))
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(onPressed: (){
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => RegPage()));
+                    }, child: Text("注册账户",style: TextStyle(fontWeight: FontWeight.w400,color: Colors.blue[500]),)),
+                    FlatButton(onPressed: (){}, child: Text("忘记密码",style: TextStyle(fontWeight: FontWeight.w400,color: Colors.black54),)),
                   ],
-                ))
+                ),
+              ),
+            ],
           ),
+          Column(
+            children: <Widget>[
+              Text('第三方社交账号登陆', style: TextStyle(fontSize: 12,color: Colors.black38),),
+              Padding(padding: EdgeInsets.only(top: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  FlatButton(onPressed: (){},
+                    child: Column(
+                    children: <Widget>[
+                      Icon(IconData(0xe619,fontFamily: 'media'),size: 38,color: Colors.green,),
+                      Text("微信",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w300),)
+                    ],
+                    ),
+                  ),
+                  FlatButton(onPressed: (){},
+                    child: Column(
+                      children: <Widget>[
+                        Icon(IconData(0xe63c,fontFamily: 'media'),size: 38,color: Colors.blue),
+                        Text("QQ",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w300),)
+                      ],
+                    ),
+                  ),
+                  FlatButton(onPressed: (){},
+                    child: Column(
+                      children: <Widget>[
+                        Icon(IconData(0xe618,fontFamily: 'media'),size: 38,color: Colors.red),
+                        Text("微博",style: TextStyle(fontSize: 12,fontWeight: FontWeight.w300),)
+                      ],
+                    ),
+                  ),
+                ],
+              ),)
+            ],
+          )
         ],
       ),
     );
